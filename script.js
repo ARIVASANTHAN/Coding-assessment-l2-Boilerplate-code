f0c9b1952596052ea058da7a1bddf55c54483476
ARIVASANTHAN <166494175+ARIVASANTHAN@users.noreply.github.com>
2024-04-26 15:38:05 +0530

    Update script.js
    I am updated my JavaScript file

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+function showImages(category) {
+    var columns = document.querySelectorAll('.image-column');
+    columns.forEach(function(column) {
+        if (column.classList.contains(category)) {
+            column.style.display = 'block';
+        } else {
+            column.style.display = 'none';
+        }
+    });
+}
